cc6f248d6612ce34755bbe8189f7e1654281b230
Kenwoll <i.murad.2003@gmail.com>
2023-12-02 19:25:47 +0400

    FEATURE: select and insert

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"unsafe"
 )
 
 type MetaCommandResult int
@@ -18,6 +19,7 @@ type PrepareResult int
 
 const (
 	PREPARE_SUCCESS                PrepareResult = iota
+	PREPARE_SYNTAX_ERROR
 	PREPARE_UNRECOGNIZED_STATEMENT
 )
 
@@ -28,12 +30,49 @@ const (
 	STATEMENT_SELECT
 )
 
+type ExecuteResult int
+
+const (
+	EXECUTE_SUCCESS	   ExecuteResult = iota
+	EXECUTE_TABLE_FULL
+	EXECUTE_UNRECOGNIZED_STATEMENT
+)
+
+const (
+	TABLE_MAX_PAGES = 100
+	PAGE_SIZE = 4096
+	ROW_SIZE = unsafe.Sizeof(Row{})
+	ROWS_PER_PAGE = int(PAGE_SIZE / ROW_SIZE)
+	TABLE_FULL_SIZE = int(PAGE_SIZE * TABLE_MAX_PAGES)
+) 
+
 type Statement struct {
 	Type StatementType
+	Row_to_insert Row
 }
+
+type Row struct {
+	ID       uint32
+	Username [32]byte
+	Email    [255]byte
+}
+
+type Page struct {
+	Rows [ROWS_PER_PAGE]Row
+}
+
+type Table struct {
+	NumRows int
+	Pages   [TABLE_MAX_PAGES]*Page
+}
+
 
 func printPrompt() {
 	fmt.Print("db > ")
+}
+
+func printRow(row *Row) {
+	fmt.Printf("(%d, %s, %s)\n", row.ID, row.Username, row.Email)
 }
 
 func readInput(reader *bufio.Reader) (string, error) {
@@ -45,6 +84,29 @@ func readInput(reader *bufio.Reader) (string, error) {
 	input = strings.TrimSpace(input)
 
 	return input, nil
+}
+
+func rowSlot(table *Table, rowNum int) *Row {
+	pageNum := rowNum / ROWS_PER_PAGE
+	rowOffset := rowNum % ROWS_PER_PAGE
+
+	// Check if the page exists
+	if pageNum >= len(table.Pages) || table.Pages[pageNum] == nil {
+		// Allocate memory only when we try to access the page
+		table.Pages[pageNum] = &Page{}
+	}
+
+	return &table.Pages[pageNum].Rows[rowOffset]
+}
+
+func create_Table() *Table {
+	table := &Table{}
+	table.NumRows = 0
+	for i := 0; i < TABLE_MAX_PAGES; i++ {
+		table.Pages[i] = nil
+	}
+
+	return table
 }
 
 func executeMetaCommand(input string) MetaCommandResult {
@@ -59,6 +121,21 @@ func executeMetaCommand(input string) MetaCommandResult {
 func prepareStatement(input string, statement *Statement) PrepareResult {
 	if strings.HasPrefix(input, "insert") {
 		statement.Type = STATEMENT_INSERT
+
+		var username, email string
+
+		args, err := fmt.Sscanf(input, "insert %d %s %s", &statement.Row_to_insert.ID, &username, &email)
+		if err != nil {
+			return PREPARE_SYNTAX_ERROR
+		}
+
+		if args != 3 {
+			return PREPARE_SYNTAX_ERROR
+		}
+
+		copy(statement.Row_to_insert.Username[:], username)
+    	copy(statement.Row_to_insert.Email[:], email)
+
 		return PREPARE_SUCCESS
 	}
 	if strings.Compare(input, "select") == 0 {
@@ -69,19 +146,46 @@ func prepareStatement(input string, statement *Statement) PrepareResult {
 	return PREPARE_UNRECOGNIZED_STATEMENT
 }
 
-func executeStatement(Statement *Statement) {
+func execute_insert(statement *Statement, table *Table) ExecuteResult {
+	if table.NumRows >= TABLE_FULL_SIZE {
+		return EXECUTE_TABLE_FULL
+	}
+
+	row := &statement.Row_to_insert
+	rowSlot := rowSlot(table, table.NumRows)
+	rowSlot.ID = row.ID
+	copy(rowSlot.Username[:], row.Username[:])
+	copy(rowSlot.Email[:], row.Email[:])
+	table.NumRows += 1
+
+	return EXECUTE_SUCCESS
+}
+
+func execute_select(statement *Statement, table *Table) ExecuteResult {
+	row := Row{}
+	for i := 0; i < table.NumRows; i++ {
+		row = *rowSlot(table, i)
+		printRow(&row)
+	}
+
+	return EXECUTE_SUCCESS
+}
+
+func executeStatement(Statement *Statement, table *Table) ExecuteResult {
 	switch Statement.Type {
 	case STATEMENT_SELECT:
-		fmt.Println("This is were we will apply select")
-		break
+		return execute_select(Statement, table)
 	case STATEMENT_INSERT:
-		fmt.Println("This is were we will apply insert")
-		break
+		return execute_insert(Statement, table)
+	default:
+		return EXECUTE_UNRECOGNIZED_STATEMENT
 	}
 }
 
 func main() {
+	table := create_Table()
 	reader := bufio.NewReader(os.Stdin)
+	
 	for {
 		printPrompt()
 		input, err := readInput(reader)
@@ -104,14 +208,22 @@ func main() {
 		switch prepareStatement(input, &statement) {
 		case PREPARE_SUCCESS:
 			break
+		case PREPARE_SYNTAX_ERROR:
+			fmt.Printf("Syntax error. Could not parse command\n")
+			continue
 		case PREPARE_UNRECOGNIZED_STATEMENT:
 			fmt.Printf("Unrecognized command '%s'\n", input)
 			continue
 		}
 	
 		
-		executeStatement(&statement);
-	    fmt.Printf("Executed.\n")
-	
+		switch executeStatement(&statement, table) {
+		case EXECUTE_SUCCESS:
+			fmt.Printf("Executed.\n")
+			break
+		case EXECUTE_TABLE_FULL:
+			fmt.Printf("Table is full.\n")
+			break
+		}
 	}
 }
